8e5bd9bd0b10a67082b5dfdb68eb1eeba066f9c8
Jian Liu <provevis@163.com>
2018-04-26 21:20:56 +0800

    update

--- session.py
+++ session.py
@@ -2,63 +2,64 @@ import json
 import vtk
 import abc
 import graph
-
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QMainWindow, QVBoxLayout, QAction, QMenu
-from PyQt5.QtGui import QCursor
-from PyQt5 import QtCore
-# from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
-from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+import sys
+from viewers import treeviewer, digraphviewer
+# from PyQt5 import QtGui
+# from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QMainWindow, QVBoxLayout, QAction, QMenu
+# from PyQt5.QtGui import QCursor
+# from PyQt5 import QtCore
+# from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 class Session:
     def __init__(self, descr, graphType, attributes):
         self.descr = descr
         self.graphType = graphType
         self.attributes = attributes
-        if graphType == 'Tree':
-            self.viewer = TreeViewer(attributes)
-        elif graphType == 'DiGraph':
-            self.viewer = DiGraphViewer(attributes)
-        else:
-            print ("Unknown graph type:", graphType)
-    
+        # if graphType == 'Tree':
+        #     self.viewer = TreeViewer(attributes)
+        # elif graphType == 'DiGraph':
+        #     self.viewer = DiGraphViewer(attributes)
+        # else:
+        #     print ("Unknown graph type:", graphType, 'exit!')
+        #     sys.exit(1)
+    @abc.abstractmethod
     def parseJSON(self, data):
         pass
-
+    @abc.abstractmethod
     def showViewer(self):
         pass
-    
-    def hideViewer(self):
+    @abc.abstractmethod
+    def closeViewer(self):
         pass
 
-class Viewer:
-    @abc.abstractmethod
-    def parseJSON(self, data):
-        pass
-    @abc.abstractmethod
-    def showView(self):
-        pass
-    @abc.abstractmethod
-    def hideView(self):
-        pass
-
-class TreeViewer(Viewer):
-    def __init__(self, attributes):
+class TreeSession(Session):
+    def __init__(self, descr, attributes):
+        super(TreeSession, self).__init__(descr, 'Tree', attributes)
         self.tree = graph.Tree(attributes)
+        self.viewer = treeviewer.TreeViewer()
+    
     def parseJSON(self, data):
         pass
-    def showView(self):
-        pass
-    def hideView(self):
-        pass
+    
+    def showViewer(self):
+        self.viewer.show()
 
-class DiGraphViewer(Viewer):
-    def __init__(self, attributes):
+    def closeViewer(self):
+        self.viewer.close()
+
+class DiGraphSession(Session):
+    def __init__(self, descr, attributes):
+        super(DiGraphSession, self).__init__(descr, 'DiGraph', attributes)
         self.digraph = graph.DiGraph(attributes)
-        
+        self.viewer = digraphviewer.DiGraphViewer()
+    
     def parseJSON(self, data):
         pass
-    def showView(self):
-        pass
-    def hideView(self):
-        pass
+    
+    def showViewer(self):
+        self.viewer.show()
+
+    def closeViewer(self):
+        self.viewer.close()
+
+
---
+++ viewers/treeviewer.py
@@ -0,0 +1,11 @@
+
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QMainWindow, QVBoxLayout, QAction, QMenu
+from PyQt5.QtGui import QCursor
+from PyQt5 import QtCore
+
+class TreeViewer(QMainWindow):
+    def __init__(self):
+        self.setWindowTitle('Tree')
+        pass
+    
